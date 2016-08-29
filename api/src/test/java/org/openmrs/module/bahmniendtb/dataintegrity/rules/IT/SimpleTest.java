79ed9d498e5d98231f174266cb67c5bed4a81a8e
Deepak Sharma <deepaucksharma@gmail.com>
2016-08-29 12:57:46 +0530

    Deepak | #000 | Add dependency for Integration Tests.

--- api/src/test/java/org/openmrs/module/bahmniendtb/dataintegrity/rules/IT/SimpleTest.java
+++ api/src/test/java/org/openmrs/module/bahmniendtb/dataintegrity/rules/IT/SimpleTest.java
@@ -8,10 +8,9 @@ import org.openmrs.test.BaseModuleContextSensitiveTest;
 import javax.validation.constraints.AssertTrue;
 
 @org.springframework.test.context.ContextConfiguration(locations = {"classpath:TestingApplicationContext.xml"}, inheritLocations = true)
-public class SimpleTest {
+public class SimpleTest extends BaseModuleContextSensitiveTest{
     @Test
     public void name() throws Exception {
         Assert.assertTrue(true);
-
     }
 }
